32ceaa0e01a402d7da9a169984b559b0dfab54eb
NicoRizz16 <nicolas.rizzon@gmail.com>
2018-10-27 15:41:21 +0200

    add homepage visitor

--- src/AppBundle/Controller/Visitor/RessourcesController.php
+++ src/AppBundle/Controller/Visitor/RessourcesController.php
@@ -6,16 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends Controller
+class RessourcesController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('visitor/ressources/index.html.twig');
     }
 }
